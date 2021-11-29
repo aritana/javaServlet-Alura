68d596b34ac2fda7a7b3fd88e76aa9312416ecc3
Aritana Noara <arinoara@yahoo.com.br>
2021-11-28 21:16:14 -0300

    adicionando bibliotecas http-client e jetty

--- src/main/java/gerenciador/EmpresasService.java
+++ src/main/java/gerenciador/EmpresasService.java
@@ -22,20 +22,35 @@ import gerenciador.modelo.Empresa;
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		List<Empresa> empresas = new Banco().getEmpresas();
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);  
+		String type = request.getHeader("Accept");
+		System.out.println(type);
 		
-	/*	List<Empresa> empresas = new Banco().getEmpresas();
-		XStream xstream = new XStream();
-		xstream.alias("empresa", Empresa.class);
-		String xml = xstream.toXML(empresas);  */
-	
-		
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
+		if (type.contains("xml")) {
+			System.out.println("processando via xml");
+			List<Empresa> empresas = new Banco().getEmpresas();
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+
+		} else if (type.contains("json")) {
+			System.out.println("processando via json");
+			List<Empresa> empresas = new Banco().getEmpresas();
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+			
+		}
+		else {
+			response.setContentType("application/json");
+			response.getWriter().print("'message': 'no content'");
+		}
+
 	}
 
 }
